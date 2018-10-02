934be31de8b6600f3c00714342a2deb9a94e14bd
Mike Wojtyna <mike@buildmysoftware.pro>
2018-10-02 15:53:24 +0200

    Init fresh project

---
+++ src/test/java/pro/buildmysoftware/testlimits/TestLimitsApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.buildmysoftware.testlimits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestLimitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
